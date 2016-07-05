015bf70dddab7030de5ff949757dad13448f60ef
Jamie <JamieChien.C@gmail.com>
2016-07-05 21:15:48 +0800

    使用 目標類別中的方法產生測試專案及測試類別

---
+++ TDDHomework/TDDHomeworkTests/TargetTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDHomework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDHomework.Tests
+{
+    [TestClass()]
+    public class TargetTests
+    {
+        [TestMethod()]
+        public void SubTotalTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
